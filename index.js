3013ab16310d210412fd4053d839ef4c1246d627
Detilly <bvjulien@gmail.com>
2021-09-02 10:46:43 -0400

    'ajustement'

--- index.js
+++ index.js
@@ -1,25 +1,56 @@
 const fs = require('fs');
 const path = require('path');
 const Logger = require('./logger');
-const monJournal = new Logger();
+let monJournal = new Logger( );
 
-function toto(data) {
-    console.log('avertissement:', data);
+monJournal.on('avertissement', (data) => eventAvertissement(data));
+monJournal.on('information', (data) => eventInformation(data));
+monJournal.on('erreur', (data) => eventErreur(data));
+
+function eventAvertissement(data) {
     fs.appendFile(
-        path.join(__dirname, 'test', 'salut.txt'),
-        `avertissement: { \n id: ${data.id}, temps: ${data.temps} },`,
-        err => {
-            if (err) throw err;
-        }
+        path.join(__dirname, './logs/messages.log',),
+            `avertissement: { \n id: ${data.id}, temps: ${data.temps}, message: ${data.message},  usager: ${data.usager} },`,
+                err => {
+                    if (err) throw err;
+                }
+        
     )
 }
 
-monJournal.on('avertissement', (data) => toto(data));
-monJournal.on('information', (data) => console.log('information:', data));
-monJournal.on('erreur', (data) => console.log('monJournal erreur:', data));
+function eventErreur(data) {
+    fs.appendFile(
+        path.join(__dirname, './logs/erreurs.log'),
+            `erreur: { \n id: ${data.id}, temps: ${data.temps}, erreur: ${data.erreur}, usager: ${data.usager} },`,
+                err => {
+                    if (err) throw err;
+                }
+        
+    )
+}
 
-monJournal.on('message', (data) => console.log('monJournal msg: ', data));
+function eventInformation(data) {
+    fs.appendFile(
+        path.join(__dirname, './logs/informations.log'),
+            `information: { \n id: ${data.id}, temps: ${data.temps}, info: ${data.info}, usager: ${data.usager} },`,
+                err => {
+                    if (err) throw err;
+                }
+        
+    )
+}
 
-monJournal.log('ALLO?????????');
-monJournal.log('HEY...');
-
+monJournal.log('joseph', 'Salut');
+monJournal.error('bob', 'voici l erreur');
+monJournal.log('joseph', '2e message');
+monJournal.log('joseph', 'encore kekchose');
+monJournal.info('lucie', 'autre truc');
+monJournal.error('bob', 'voici l erreur');
+monJournal.log('joseph', '2e message');
+monJournal.log('joseph', 'encore kekchose');
+monJournal.info('lucie', 'autre truc');
+monJournal.error('sylvie', 'erreur le fichier est introuvable');
+monJournal.log('marc', 'le dossier a ete cree');
+monJournal.log('joseph', 'le fichier a ete renomme');
+monJournal.info('lucie', 'les permissions ont ete changees');
+monJournal.error('paul', 'la base de donnees a ete supprimee');
--- logger.js
+++ logger.js
@@ -2,9 +2,19 @@ const uuid = require('uuid');
 const EventEmitter = require('events');
 
 class Logger extends EventEmitter {
-    log(msg) {
+    log(nom, message) {
         let maDate = new Date().toLocaleString();
-        this.emit('message', { id: uuid.v4(), temps: maDate, msg: msg });
+        this.emit('avertissement', { id: uuid.v4(), temps: maDate, message: message, usager: nom });
+    }
+
+    error(nom, message) {
+        let maDate = new Date().toLocaleString();
+        this.emit('erreur', { id: uuid.v4(), temps: maDate, erreur: message, usager: nom });
+    }
+
+    info(nom, message) {
+        let maDate = new Date().toLocaleString();
+        this.emit('information', { id: uuid.v4(), temps: maDate, info: message, usager: nom });
     }
 }
 
